d94766e3fd8673f05b20ff53c50980c61d84f852
Roman Zhikharevich <rzhikharevich@gmail.com>
2020-09-10 10:04:31 +0300

    Basic project structure

---
+++ xmem/range.h
@@ -0,0 +1,24 @@
+#ifndef XMEM_RANGE_H
+#define XMEM_RANGE_H
+
+#include <utility>
+
+
+namespace xmem {
+
+
+// template <typename Iterator>
+// class Range {
+// public:
+//     using value_type = decltype(*std::declval<Iterator>());
+//     using reference = value_type&;
+//     using const_reference = const value_type&;
+//     using iterator = Iterator;
+// };
+
+
+}
+
+
+
+#endif
